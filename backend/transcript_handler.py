134847dd34cbe7978f706f98c3a848e5fe919dcd
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2025-11-01 10:07:01 +0530

    Update transcript_handler

--- backend/transcript_handler.py
+++ backend/transcript_handler.py
@@ -17,21 +17,19 @@ def extract_video_id(url):
 def get_transcript(video_id):
     """Get transcript for a YouTube video"""
     try:
-        # Try multiple language codes
-        language_codes = ['en', 'en-US', 'en-GB']
         
-        for lang_code in language_codes:
-            try:
-                transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=[lang_code])
-                transcript_text = ' '.join([item['text'] for item in transcript_list])
-                return transcript_text
-            except:
-                continue
-        
-        # If specific language codes fail, try getting any available transcript
         transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
         transcript_text = ' '.join([item['text'] for item in transcript_list])
         return transcript_text
         
     except Exception as e:
-        raise Exception(f"Could not retrieve transcript: {str(e)}")
+        
+        try:
+            transcript_list = YouTubeTranscriptApi.get_transcript(
+                video_id, 
+                languages=['en', 'en-US', 'en-GB']
+            )
+            transcript_text = ' '.join([item['text'] for item in transcript_list])
+            return transcript_text
+        except Exception as lang_error:
+            raise Exception(f"Could not retrieve transcript: {str(e)}")
